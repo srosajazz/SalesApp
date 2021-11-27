89cc57ebf383978aea40d600ee33e8401f886dfb
Sergio Rosa <scarlosjazz@gmail.com>
2021-11-27 17:24:32 -0500

    Home, navigation - LINK

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,29 @@
+import Footer from 'components/Footer'
+import NavBar from 'components/NavBar'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className='container'>
+        <div className='jumbotron'>
+          <h1 className='display-4'>ENSDash</h1>
+          <p className='lead'>
+            Analise dos estudantes e diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            This application consists of displaying a dashboard from data
+            provided by a back end built with Spring Boot
+          </p>
+          <Link className='btn btn-primary btn-lg' to='/dashboard'>
+            Access Student Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
