bfb53764090d2c585f3deebf3715a900b83c6c9a
Sergio Rosa <scarlosjazz@gmail.com>
2021-11-28 18:11:24 -0500

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -11,7 +11,7 @@ const DonutChart = () => {
   //wrong way
   let chartData: ChartData = { labels: [], series: [] }
 
-  axios.get(`${BASE_URL}sales/amount-by-seller`).then((response) => {
+  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
     const data = response.data as SaleSum[]
     const myLabels = data.map((x) => x.sellerName)
     const mySeries = data.map((x) => x.sum)
